e109871e5ec265f197d033e7e301820ef4afba35
darrenmuliawan <darrenm2@illinois.edu>
2023-03-01 13:12:18 +0700

    add horizontal scroll behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { About, Canvas, Introduction } from './ui'
+import { Articles } from './ui/Articles'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    // set scroll to horizontal
+    const app = document.getElementById('app');
+    app?.addEventListener("wheel", (e) => {
+      if (e.deltaY > 0) {
+        app.scrollLeft += 30;
+        e.preventDefault();
+      } else {
+        app.scrollLeft -= 30;
+        e.preventDefault();
+      }
+    })
+  }, [])
+
   return (
-    <div className="app">
+    <div id='app' className="app">
       <Canvas>
         <Introduction />
         <About />
+        <Articles />
+      </Canvas>
+      <Canvas>
+        <Introduction />
+        <About />
+        <Articles />
       </Canvas>
     </div>
   )
---
+++ src/ui/Articles/Articles.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Articles = () => {
+  return (
+    <div
+      className='w-[420px]'
+    >
+      <h1>My writings</h1>
+    </div>
+  )
+}
