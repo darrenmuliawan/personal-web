1da435252223a5ada4a3f3ff653b7c08812ccbdb
darrenmuliawan <darrenm2@illinois.edu>
2023-03-01 21:13:19 +0700

    update experience page

--- src/ui/Experience/Experience.tsx
+++ src/ui/Experience/Experience.tsx
@@ -7,6 +7,8 @@ export const Experience = () => {
       className='mx-10 flex'
     >
       <div className='mx-10 flex flex-col'>
+        <h1 className='text-center font-bold'>8</h1>
+        <h1 className='text-center font-bold my-10'>-</h1>
         <h1 className='text-center font-bold'>2</h1>
         <h1 className='text-center font-bold'>0</h1>
         <h1 className='text-center font-bold'>1</h1>
@@ -24,8 +26,10 @@ export const Experience = () => {
       </div>
       <div className='mx-10 relative'>
         <div
-          className='absolute top-1/4 flex flex-col -mt-[6em]'
+          className='absolute flex flex-col'
         >
+          <h1 className='text-center font-bold'>8</h1>
+          <h1 className='text-center font-bold my-10'>-</h1>
           <h1 className='text-center font-bold'>2</h1>
           <h1 className='text-center font-bold'>0</h1>
           <h1 className='text-center font-bold'>1</h1>
@@ -38,9 +42,192 @@ export const Experience = () => {
           className='brightness-50 min-w-[690px] max-w-[690px] mb-4'
         />
         <h1 className='underline mb-10'>transfer to uni</h1>
-        <p className='mb-4'>on august 4th 2015, i was leaving my hometown Surabaya, Indonesia with a one-way ticket to San Francisco, California. after graduated high school, i decided to study at <a>Diablo Valley College</a> in Pleasant Hill, California.</p>
-        <p className='mb-4'>i was planning to study mechanical engineering initially, but things changed after i took my first computer science class. i liked cs better than physics/chemistry because i thought it was "easier". i thought to myself "hmm i struggled a lot and got a B on my first physics class, whereas i aced my first computer science class with very little prior programming knowledge, maybe i should change major and start coding. i think cs is much easier."</p>
-        <p className='mb-4'>boy oh boy was i wrong, coding <strong><u>is not</u></strong> easy (don't get me wrong, i enjoy every struggle that i had learning to code tho :D)</p>
+        <p className='mb-4'>fast forward to 2017, i transferred from college to <a>University of Illinois at Urbana-Champaign</a> majoring in computer science & statistics as a junior. did i say cs @ college was hard? lol it's even harder in uni because 1) uni is generally tougher than community college (just harder and more workload) and 2) UIUC is one of the best computer science school, notable alumni includes <a>Marc Andreessen</a>. <a>Max Levchin (co-founder of Paypal)</a>, and <a>Thomas Siebel (founder of C3.ai)</a>.</p>
+        <p className='mb-4'>at this point, i'm in a love & hate relationship with coding. it's hard, but it challenges you to think creatively.</p>
+      </div>
+      <div className='mx-10 relative'>
+        <div
+          className='absoluteflex flex-col'
+        >
+          <h1 className='text-center font-bold'>5</h1>
+          <h1 className='text-center font-bold my-10'>-</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>0</h1>
+          <h1 className='text-center font-bold'>1</h1>
+          <h1 className='text-center font-bold'>9</h1>
+        </div>
+      </div>
+      <div className='mx-10 min-w-[690px] max-w-[690px]'>
+        <img 
+          src={GoldenGate} 
+          className='brightness-50 min-w-[690px] max-w-[690px] mb-4'
+        />
+        <h1 className='underline mb-10'>graduation</h1>
+        <p className='mb-4'>i finally graduated from UIUC in 2019 with a GPA of 3.69 (not bad, huh?). however, i was unable to find a software engineering job in the midwest area. i thought midwest have less opportunity for software engineers than bay area, so i decided to book a one-way ticket to san francisco, california.</p>
+      </div>
+      <div className='mx-10 relative'>
+        <div
+          className='absoluteflex flex-col'
+        >
+          <h1 className='text-center font-bold'>8</h1>
+          <h1 className='text-center font-bold my-10'>-</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>0</h1>
+          <h1 className='text-center font-bold'>1</h1>
+          <h1 className='text-center font-bold'>9</h1>
+        </div>
+      </div>
+      <div className='mx-10 min-w-[690px] max-w-[690px]'>
+        <img 
+          src={GoldenGate} 
+          className='brightness-50 min-w-[690px] max-w-[690px] mb-4'
+        />
+        <h1 className='underline mb-10'>my first job</h1>
+        <p className='mb-4'>after 1 month of crashing on my college friends' couch, i met 2 guys who were working in a healthcare industry and they were looking for a frontend engineer to help them. then i joined <a>Dialisa</a>, a healthcare startup which goal is to improve the quality of life of patients with CKD (chronic kidney disease).</p>
+      </div>
+      <div className='mx-10 relative'>
+        <div
+          className='absoluteflex flex-col'
+        >
+          <h1 className='text-center font-bold'>8</h1>
+          <h1 className='text-center font-bold my-10'>-</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>0</h1>
+          <h1 className='text-center font-bold'>1</h1>
+          <h1 className='text-center font-bold'>9</h1>
+        </div>
+      </div>
+      <div className='mx-10 min-w-[690px] max-w-[690px]'>
+        <img 
+          src={GoldenGate} 
+          className='brightness-50 min-w-[690px] max-w-[690px] mb-4'
+        />
+        <h1 className='underline mb-10'>master's degree</h1>
+        <p className='mb-4'>during this time, i felt that i don't know a lot of stuffs and felt i need more education. i decided to take a master's degree (through MOOC) on data science from UIUC while i'm working full-time at Dialisa.</p>
+      </div>
+      <div className='mx-10 relative'>
+        <div
+          className='absolute flex flex-col'
+        >
+          <h1 className='text-center font-bold'>5</h1>
+          <h1 className='text-center font-bold my-10'>-</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>0</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>1</h1>
+        </div>
+      </div>
+      <div className='mx-10 min-w-[690px] max-w-[690px]'>
+        <img 
+          src={GoldenGate} 
+          className='brightness-50 min-w-[690px] max-w-[690px] mb-4'
+        />
+        <h1 className='underline mb-10'>graduation (again)</h1>
+        <p className='mb-4'>i finished my master's degree from UIUC with a GPA of 3.90. imo, master's degree is <strong><u>way easier</u></strong> than bachelor's, probably because of the major too. however, while studying data science and working as a software engineer, i feel that i like being a software engineer rather than a data scientist. however, there's 1 very important lesson that i learn this year is that <strong><u>nobody knows anything</u></strong>, like for real. growing up, i have this beliefs where most working adults know their stuffs. turns out they are also figuring things out, but with more experience.</p>
+      </div>
+      <div className='mx-10 relative'>
+        <div
+          className='absolute flex flex-col'
+        >
+          <h1 className='text-center font-bold'>1</h1>
+          <h1 className='text-center font-bold my-10'>-</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>0</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>2</h1>
+        </div>
+      </div>
+      <div className='mx-10 min-w-[690px] max-w-[690px]'>
+        <img 
+          src={GoldenGate} 
+          className='brightness-50 min-w-[690px] max-w-[690px] mb-4'
+        />
+        <h1 className='underline mb-10'>moving back to Indonesia</h1>
+        <p className='mb-4'>after 6.5 years in the States, i decided to move back to indonesia for a family & personal reason. i have mixed feelings about this, USA has been my home for a while and leaving was not easy at all. however, i thought that it was the best decision at the time.</p>
+      </div>
+      <div className='mx-10 relative'>
+        <div
+          className='absolute flex flex-col'
+        >
+          <h1 className='text-center font-bold'>3</h1>
+          <h1 className='text-center font-bold my-10'>-</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>0</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>2</h1>
+        </div>
+      </div>
+      <div className='mx-10 min-w-[690px] max-w-[690px]'>
+        <img 
+          src={GoldenGate} 
+          className='brightness-50 min-w-[690px] max-w-[690px] mb-4'
+        />
+        <h1 className='underline mb-10'>moving on...</h1>
+        <p className='mb-4'>Dialisa has been slowing down for the past year. it's very hard to break into a healthcare industry, an industry with a very tight regulation and a lot of gate-keepers. one of the reason i go back to Indonesia is because we think maybe we can sell better in Indonesia. however, that's not the case. it was a hard decision, but i decided to move on.</p>
+        <p className='mb-4'>i decided that i want to join a company who is working in web3 (because it's the fUtURe). crypto has been moving downward since the beginning of the 2022, but i always thought bear market was a good time to build. i joined <a href="https://www.inflectionpoints.co/">Inflection Points</a> as a founding frontend engineer, a startup based in Miami, Florida who works in training common people and professionals to learn more about web3 as an industry.</p>
+      </div>
+      <div className='mx-10 relative'>
+        <div
+          className='absolute flex flex-col'
+        >
+          <h1 className='text-center font-bold'>3</h1>
+          <h1 className='text-center font-bold my-10'>-</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>0</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>2</h1>
+        </div>
+      </div>
+      <div className='mx-10 min-w-[690px] max-w-[690px]'>
+        <img 
+          src={GoldenGate} 
+          className='brightness-50 min-w-[690px] max-w-[690px] mb-4'
+        />
+        <h1 className='underline mb-10'>after hours & weekend side hustle</h1>
+        <p className='mb-4'>i also joined <a href="https://joltzrewards.com/">Joltz</a>, a startup who wants to bring easy bitcoin-as-a-reward integration to merchants, at this time as part-time contributor.</p>
+      </div>
+      <div className='mx-10 relative'>
+        <div
+          className='absolute flex flex-col'
+        >
+          <h1 className='text-center font-bold'>1</h1>
+          <h1 className='text-center font-bold my-10'>-</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>0</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>3</h1>
+        </div>
+      </div>
+      <div className='mx-10 min-w-[690px] max-w-[690px]'>
+        <img 
+          src={GoldenGate} 
+          className='brightness-50 min-w-[690px] max-w-[690px] mb-4'
+        />
+        <h1 className='underline mb-10'>the unexpected</h1>
+        <p className='mb-4'>after less than a year and crazy market downturn especially in crypto, I was laid off from <a href="https://www.inflectionpoints.co/">Inflection Points</a>. i was pretty shocked by the decision, but it is what it is.</p>
+        <p className='mb-4'>i decided to take some time off from job search and take this opportunity to spend more time with my family.</p>
+      </div>
+      <div className='mx-10 relative'>
+        <div
+          className='absolute flex flex-col'
+        >
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold my-10'>-</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>0</h1>
+          <h1 className='text-center font-bold'>2</h1>
+          <h1 className='text-center font-bold'>3</h1>
+        </div>
+      </div>
+      <div className='mx-10 min-w-[690px] max-w-[690px]'>
+        <img 
+          src={GoldenGate} 
+          className='brightness-50 min-w-[690px] max-w-[690px] mb-4'
+        />
+        <h1 className='underline mb-10'>new beginning</h1>
+        <p className='mb-4'>after taking a few weeks time-off, i am now back to the grind. here i am creating this personal website, telling a random person/future employer about my experience so far.</p>
+        <p className='mb-4'>i am currently looking for a new opportunity as a full-stack or frontend engineer. you can see my resume <a>here</a> or you can contact me at <a>darrenmuliawan8@gmail.com</a>.</p>
+        <p className='mb-4'>this is the end of my story, for now. hope you enjoy it :)</p>
       </div>
     </div>
   )
--- src/ui/Introduction/Introduction.tsx
+++ src/ui/Introduction/Introduction.tsx
@@ -14,7 +14,7 @@ export const Introduction = () => {
         <p className='mb-4'>hello world, welcome to my personal website! where I showcase my skills in frontend development and unique design techniques. As a frontend developer, I strive to create visually stunning and highly functional websites that engage users and provide an intuitive user experience. My passion for design and development has allowed me to hone my skills in various areas such as HTML, CSS, JavaScript, and React, to name a few.</p>
         <p className='mb-4'>on this website, you will find a collection of my best work, including web design projects, case studies, and personal insights into my design process. I believe in creating user-centric designs that not only look great but also provide a seamless experience for users. With my skills in frontend development, I aim to create responsive and optimized websites that are accessible to all users, regardless of their device or platform. I invite you to explore my portfolio and see for yourself the unique design skills and frontend development expertise I bring to the table.</p>
         <button>
-          click this for "normal view" :)
+          click this for "simple view" :)
         </button>
       </div>
       <div
